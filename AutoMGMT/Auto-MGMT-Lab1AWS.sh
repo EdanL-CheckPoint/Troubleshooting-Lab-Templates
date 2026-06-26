8c0b9deee47502f2a80ddcaf6e58be10d48c0c5f
Edan Leventhal <edanl@checkpoint.com>
2026-06-26 15:55:37 +0300

    aws lab mgmt script 1

--- AutoMGMT/Auto-MGMT-Lab1AWS.sh
+++ AutoMGMT/Auto-MGMT-Lab1AWS.sh
@@ -7,8 +7,15 @@ mgmt_cli login --root true > id.txt
 mgmt_cli set access-rule name "Cleanup rule" layer "Network" action "Accept" -s id.txt --format json
 
 #Adding Network objects for Web server and App server subnets
-mgmt_cli add network name "Web-Network" subnet "192.168.1.0/24" -s id.txt --format json
-mgmt_cli add network name "App-Network" subnet "192.168.2.0/24" -s id.txt --format json
+#mgmt_cli add network name "Web-Network" subnet "10.1.1.0" subnet-mask "255.255.255.0" -s id.txt --format json
+#mgmt_cli add network name "App-Network" subnet "10.1.2.0" subnet-mask "255.255.255.0" -s id.txt --format json
+
+#Adding web and app servers
+mgmt_cli add host name "Web-Server" ip-address "10.1.1.10" -s id.txt --format json
+mgmt_cli add host name "App-Server" ip-address "10.1.2.10" -s id.txt --format json
+
+#Adding access rules
+mgmt_cli add access-rule layer "Network" position "1" name "Allow Web to App" source.1 "Web-Server" source.2 "App-Server" destination.1 "Web-Server" destination.2 "App-Server" service.1 "SSH" service.2 "icmp-proto" action "Accept" -s id.txt --format json
 
 #Publish
 mgmt_cli publish -s id.txt
