6823321f087871b0d9556380ab02edd44bb73ff8
Edan Leventhal <edanl@checkpoint.com>
2026-08-07 10:51:05 +0300

    fixing auto-mmgt for aws lab

--- AutoMGMT/Auto-MGMT-Lab1AWS.sh
+++ AutoMGMT/Auto-MGMT-Lab1AWS.sh
@@ -11,8 +11,8 @@ mgmt_cli set access-rule name "Cleanup rule" layer "Network" action "Accept" -s 
 #mgmt_cli add network name "App-Network" subnet "10.1.2.0" subnet-mask "255.255.255.0" -s id.txt --format json
 
 #Adding web and app servers
-mgmt_cli add host name "Web-Server" ip-address "10.1.1.10" -s id.txt --format json
-mgmt_cli add host name "App-Server" ip-address "10.1.2.10" -s id.txt --format json
+mgmt_cli add host name "Web-Server" ip-address "10.10.0.50" -s id.txt --format json
+mgmt_cli add host name "App-Server" ip-address "10.20.0.50" -s id.txt --format json
 
 #Adding access rules
 mgmt_cli add access-rule layer "Network" position "1" name "Allow Web to App" source.1 "Web-Server" source.2 "App-Server" destination.1 "Web-Server" destination.2 "App-Server" service.1 "SSH" service.2 "icmp-proto" action "Accept" -s id.txt --format json
